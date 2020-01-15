06695c92299ef071257e5517e96836ed83712be2
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2020-01-15 22:26:29 +0100

    Created student model and created DbContext

---
+++ EntityFrameworkCore.TemporalTables.TestApi/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EntityFrameworkCore.TemporalTables.TestApi.Models
+{
+    public class Student : TemporalEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string School { get; set; }
+    }
+
+    public class TemporalEntity
+    {
+        public DateTime SysStartTime { get; set; }
+        public DateTime SysEndTime { get; set; }
+    }
+}
---
+++ EntityFrameworkCore.TemporalTables.TestApi/Repository/Context.cs
@@ -0,0 +1,26 @@
+﻿using EntityFrameworkCore.TemporalTables.Extensions;
+using EntityFrameworkCore.TemporalTables.TestApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.TemporalTables.TestApi.Repository
+{
+    public class Context : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public Context(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            // Two different ways of setting up temporal entities.
+            // First: Configure as default for all.
+            modelBuilder.UseTemporalTables(); // Create temporal table for all of your entities by default.
+
+            // Second: Table not temporal by default:
+            // modelBuilder.PreventTemporalTables(); // Do not create temporal table for none of your entities by default.
+            // modelBuilder.Entity<Student>(s => s.UseTemporalTable()); // Setup entity with temporal table.
+        }
+    }
+}
